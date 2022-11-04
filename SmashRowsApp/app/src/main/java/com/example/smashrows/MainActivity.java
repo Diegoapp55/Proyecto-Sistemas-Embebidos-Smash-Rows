0e149f5f2c29cbf9fcae5a1d50a6f9db7c5a0713
etorob <etorob@unal.edu.co>
2022-11-03 22:17:10 -0500

    Added app logo

--- SmashRowsApp/app/src/main/java/com/example/smashrows/MainActivity.java
+++ SmashRowsApp/app/src/main/java/com/example/smashrows/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    //Button to exit the application
     public void exitApp(View view) {
     finish();
     }
