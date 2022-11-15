8deab1989159498369d4d17b3a3ed12528ecaa9d
etorob <etorob@unal.edu.co>
2022-11-15 05:48:13 -0500

    InsertDocument Layout

---
+++ SmashRowsApp/app/src/main/java/com/example/smashrows/InserDocument.java
@@ -0,0 +1,14 @@
+package com.example.smashrows;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InserDocument extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inser_document);
+    }
+}
