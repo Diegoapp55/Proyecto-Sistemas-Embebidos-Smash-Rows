96e77bffa6c5a5efe1a73ca47cff79a3a36ab920
etorob <etorob@unal.edu.co>
2022-11-15 06:16:42 -0500

    Button Back Scanner

--- SmashRowsApp/app/src/main/java/com/example/smashrows/ScannerDocument.java
+++ SmashRowsApp/app/src/main/java/com/example/smashrows/ScannerDocument.java
@@ -2,7 +2,9 @@ package com.example.smashrows;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ScannerDocument extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ScannerDocument extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner_document);
+    }
+
+    //Button Back --> MainLayout
+    public void back(View view) {
+        Intent back = new Intent(this, MainActivity.class);
+        startActivity(back);
     }
 }
