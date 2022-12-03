d7f948dbdc256bbd7f96929a2b242bff34851c19
etorob <etorob@unal.edu.co>
2022-12-03 08:16:50 -0500

    Button Save Drugs Ok

--- SmashRowsApp/app/src/main/java/com/example/smashrows/ConfigDB.java
+++ SmashRowsApp/app/src/main/java/com/example/smashrows/ConfigDB.java
@@ -18,7 +18,8 @@ public class ConfigDB extends AppCompatActivity {
     EditText UserId, UserName, UserLastName, UserDrugs;
 
     //Botones Medicamentos
-
+    Button BtSaveDrugs, BtSearchDrugs, BtUpdateDrugs, BtDeleteDrugs;
+    EditText DrugsId, DrugsName, DrugsCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +38,20 @@ public class ConfigDB extends AppCompatActivity {
         UserDrugs = (EditText)findViewById(R.id.UserDrugs);
 
         //Instanciar elementos de la interfaz Medicamentos
+        BtSaveDrugs = (Button)findViewById(R.id.BtSaveDrugs);
+        BtSearchDrugs = (Button)findViewById(R.id.BtSearchDrugs);
+        BtUpdateDrugs = (Button)findViewById(R.id.BtUpdateDrugs);
+        BtDeleteDrugs = (Button)findViewById(R.id.BtDeleteDrugs);
 
+        DrugsId = (EditText)findViewById(R.id.DrugsId);
+        DrugsName = (EditText)findViewById(R.id.DrugsName);
+        DrugsCount = (EditText)findViewById(R.id.DrugsCount);
 
         //Creacion de objeto de la DB Usuarios
         HelpDBUser helpDBUser = new HelpDBUser(getApplicationContext());
 
         //Creacion de objeto de la DB Medicamentos
+        HelpDBDrugs helpDBDrugs = new HelpDBDrugs(getApplicationContext());
 
         //Botones de la DB Usuarios
         BtSaveUser.setOnClickListener(new View.OnClickListener() {
@@ -83,7 +92,41 @@ public class ConfigDB extends AppCompatActivity {
         });
 
         //Botones de la DB Medicamentos
+        BtSaveDrugs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //DB Medicamentos configurada para escribir
+                SQLiteDatabase db_drugs = helpDBDrugs.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put(HelpDBDrugs.DATATABLE.COLUMN_ID, DrugsId.getText().toString());
+                values.put(HelpDBDrugs.DATATABLE.COLUMN_DRUGS, DrugsName.getText().toString());
+                values.put(HelpDBDrugs.DATATABLE.COLUMN_COUNT, DrugsCount.getText().toString());
 
+                Long IdSaveDrugs = db_drugs.insert(HelpDBDrugs.DATATABLE.TABLE_NAME, HelpDBDrugs.DATATABLE.COLUMN_ID, values);
+                Toast.makeText(getApplicationContext(), "Se guardo el medicamento " + IdSaveDrugs, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        BtSearchUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        BtUpdateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        BtDeleteUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
